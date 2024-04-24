ad35b386bff2319dbb8ff0a9dbb8fe121ef535db
miltonzini <miltonzini@gmail.com>
2024-04-24 15:04:50 -0300

    update aside
    update icons, content and anchors

--- resources/views/partials/admin/aside.blade.php
+++ resources/views/partials/admin/aside.blade.php
@@ -1,13 +1,19 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="../../index3.html" class="brand-link">
+    <a href="{{ route('dashboard')}}" class="brand-link">
         <span class="brand-text font-weight-light">Nombre y apellido</span>
     </a>
     <div class="sidebar">
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <li class="nav-item">
+                    <a href="{{ route('dashboard')}}" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>Inicio</p>
+                    </a>
+                </li>
+                <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class="nav-icon fas fa-user"></i>
                         <p>
                             Usuarios
                             <i class="right fas fa-angle-left"></i>
@@ -15,9 +21,15 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="listado-usuarios.html" class="nav-link">
-                                <i class="far fa-circle nav-icon"></i>
+                            <a href="{{ route('admin.users.index')}}" class="nav-link">
+                                <i class="fas fa-users nav-icon"></i>
                                 <p>Todos los usuarios</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('admin.users.create')}}" class="nav-link">
+                                <i class="fas fa-plus nav-icon"></i>
+                                <p>Crear Usuario</p>
                             </a>
                         </li>
                     </ul>
