4d6986f87335a8ac41cf7d83c52b0159e6d176e7
miltonzini <miltonzini@gmail.com>
2024-04-24 13:27:40 -0300

    admin: create nav and aside modules

---
+++ resources/views/partials/admin/aside.blade.php
@@ -0,0 +1,34 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a href="../../index3.html" class="brand-link">
+        <span class="brand-text font-weight-light">Nombre y apellido</span>
+    </a>
+    <div class="sidebar">
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>
+                            Usuarios
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="listado-usuarios.html" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Todos los usuarios</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link">
+                        <i class="nav-icon fas fa-key"></i>
+                        <p>Cerrar sesión</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
