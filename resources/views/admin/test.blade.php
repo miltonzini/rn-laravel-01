7ef6e19872b7acc799b98f3ae75af475fb298b05
miltonzini <miltonzini@gmail.com>
2024-04-24 12:39:35 -0300

    create test view and route

---
+++ resources/views/admin/test.blade.php
@@ -0,0 +1,117 @@
+<x-adminLayout>
+    <div class="wrapper">
+            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <a href="../../index3.html" class="brand-link">
+                    <span class="brand-text font-weight-light">Nombre y apellido</span>
+                </a>
+                <div class="sidebar">
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p>
+                                        Usuarios
+                                        <i class="right fas fa-angle-left"></i>
+                                    </p>
+                                </a>
+                                <ul class="nav nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="listado-usuarios.html" class="nav-link">
+                                            <i class="far fa-circle nav-icon"></i>
+                                            <p>Todos los usuarios</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="index.html" class="nav-link">
+                                    <i class="nav-icon fas fa-key"></i>
+                                    <p>Cerrar sesión</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </aside>
+
+            <div class="content-wrapper">
+                <section class="content-header">
+                    <div class="container-fluid">
+                        <div class="row mb-2">
+                            <div class="col-sm-6">
+                                <h1>Usuarios</h1>
+                            </div>
+                            <div class="col-sm-6">
+                                <ol class="breadcrumb float-sm-right">
+                                    <li class="breadcrumb-item active">Listado de usuarios</li>
+                                </ol>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section class="content">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        <h3 class="card-title">Total usuarios: 0</h3>
+                                        <div class="card-tools">
+                                            <div class="input-group input-group-sm" style="width: 150px;">
+                                                <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                                                <div class="input-group-append">
+                                                    <button type="submit" class="btn btn-default">
+                                                    <i class="fas fa-search"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-body table-responsive p-0">
+                                        <table class="table table-hover text-nowrap">
+                                            <thead>
+                                                <tr>
+                                                    <th>Nombre</th>
+                                                    <th>Apellido</th>
+                                                    <th>Email</th>
+                                                    <th>Fecha de registro</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td>Juan</td>
+                                                    <td>Marini</td>
+                                                    <td>juan.marini@rednodo.com</td>
+                                                    <td>22/04/2024</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <footer class="main-footer">
+                <div class="float-right d-none d-sm-block">
+                    <b>Version</b> 1.0.0
+                </div>
+                Todos los derechos reservados.
+            </footer>
+            <aside class="control-sidebar control-sidebar-dark"></aside>
+        </div>
+
+</x-adminLayout>        
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,6 @@ Route::get('/admin', function () {return view('admin.dashboard');})->name('dashb
 
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
 Route::get('/admin/users/list', [UserController::class, 'list'])->name('admin.users.index');
+
+// Temp
+Route::get('/admin/test', function () {return view('admin.test');})->name('admin.test');
