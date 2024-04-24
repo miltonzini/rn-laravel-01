049a90c8ab0a98d174d674fbcb329bee117871d4
miltonzini <miltonzini@gmail.com>
2024-04-24 16:31:55 -0300

    update users/create view

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,60 +4,62 @@
 @include('partials.admin.aside')
 
     <main>
-        <div class="register-box">
-            <!-- <div class="register-logo">
-                <a href="registro.html"><b>Admin</b>LTE</a>
-            </div> -->
-            <div class="card">
-                <div class="card-body register-card-body">
-                    <p class="login-box-msg">Crear Usuario</p>
-                    <form action="" method="post">
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Nombre">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-user"></span>
+        <div class="content-wrapper">
+            <div class="register-box">
+                <!-- <div class="register-logo">
+                    <a href="registro.html"><b>Admin</b>LTE</a>
+                </div> -->
+                <div class="card">
+                    <div class="card-body register-card-body">
+                        <p class="login-box-msg">Crear Usuario</p>
+                        <form action="" method="post">
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Nombre">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-user"></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Apellido">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-user"></span>
+                            <div class="input-group mb-3">
+                                <input type="text" class="form-control" placeholder="Apellido">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-user"></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="email" class="form-control" placeholder="Email">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-envelope"></span>
+                            <div class="input-group mb-3">
+                                <input type="email" class="form-control" placeholder="Email">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-envelope"></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Contraseña">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-lock"></span>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control" placeholder="Contraseña">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="input-group mb-3">
-                            <input type="password" class="form-control" placeholder="Repetir contraseña">
-                            <div class="input-group-append">
-                                <div class="input-group-text">
-                                    <span class="fas fa-lock"></span>
+                            <div class="input-group mb-3">
+                                <input type="password" class="form-control" placeholder="Repetir contraseña">
+                                <div class="input-group-append">
+                                    <div class="input-group-text">
+                                        <span class="fas fa-lock"></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-12">
-                                <button type="submit" class="btn btn-primary btn-block">Crear</button>
+                            <div class="row">
+                                <div class="col-12">
+                                    <button type="submit" class="btn btn-primary btn-block">Crear</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
