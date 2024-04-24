ee0e661cd5d3e5ea4130016196f6148b053f0693
miltonzini <miltonzini@gmail.com>
2024-04-24 12:42:25 -0300

    update layout (adapt to adminLTE theme)
    Additionally, adminLTE files added to 'vendor' directory

--- resources/views/components/adminLayout.blade.php
+++ resources/views/components/adminLayout.blade.php
@@ -6,6 +6,10 @@
     <link rel="stylesheet" href="{{ mix('resources/css/app.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>RedNodo Laravel 01 | @yield('title')</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="{{ mix('vendor/adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ mix('vendor/adminlte/css/adminlte.min.css') }}">
+
 </head>
 <body>
     
@@ -13,6 +17,9 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+    <!-- <script src="{{ mix('vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script> -->
+    <!-- <script src="{{ mix('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script> -->
+    <script src="{{ mix('vendor/adminlte/js/adminlte.min.js') }}"></script>
+    <script src="{{ mix('vendor/adminlte/js/demo.js') }}"></script>
 </body>
 </html>
