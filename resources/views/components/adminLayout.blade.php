15dbb7e23865e318f0ca5975fec7f8469eab1911
miltonzini <miltonzini@gmail.com>
2024-04-25 18:01:16 -0300

    minor fix

--- resources/views/components/adminLayout.blade.php
+++ resources/views/components/adminLayout.blade.php
@@ -17,9 +17,9 @@
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <!-- <script src="{{ mix('vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script> -->
-    <!-- <script src="{{ mix('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script> -->
+    {{-- <script src="{{ mix('vendor/adminlte/plugins/jquery/jquery.min.js') }}"></script> --}}
+    {{-- <script src="{{ mix('vendor/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
     <script src="{{ mix('vendor/adminlte/js/adminlte.min.js') }}"></script>
-    <!-- <script src="{{ mix('vendor/adminlte/js/demo.js') }}"></script> -->
+    {{-- <script src="{{ mix('vendor/adminlte/js/demo.js') }}"></script> --}}
 </body>
 </html>
