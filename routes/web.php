cea26639435c66f4675319216fa8630cde52cce9
miltonzini <miltonzini@gmail.com>
2024-04-30 15:42:07 -0300

    add comments to routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 
 Route::get('/admin', function () {return view('admin.dashboard');})->name('dashboard');
 
-Route::get('/admin/users/index', [UserController::class, 'index'])->name('admin.users.index');
-Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
-Route::post('/admin/users/store', [UserController::class, 'store'])->name('admin.users.store');
-Route::get('/admin/users/edit/{id}', [UserController::class, 'edit'])->name('admin.users.edit');
+Route::get('/admin/users/index', [UserController::class, 'index'])->name('admin.users.index'); // Listar usuarios
+Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create'); // Mostrar formulario para crear usuario
+Route::post('/admin/users/store', [UserController::class, 'store'])->name('admin.users.store'); // Guardar usuario en la base de datos
+Route::get('/admin/users/edit/{id}', [UserController::class, 'edit'])->name('admin.users.edit'); // Mostrar formulario para editar usuario
 
 // Temp
 Route::get('/admin/test', function () {return view('admin.test');})->name('admin.test');
